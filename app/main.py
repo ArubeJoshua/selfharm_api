206f7b7e320f5d55ca25041e62b7da19d02aaf25
Arube Joshua <arubejoshua85@gmail.com>
2025-04-22 12:01:21 +0300

    port fix

--- app/main.py
+++ app/main.py
@@ -215,6 +215,3 @@ async def get_categories():
         }
     }
 
-# Run the application (if this file is executed directly)
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
